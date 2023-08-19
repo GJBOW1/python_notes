00a054ee5edc3ee2704ad9247846dcb265a5744b
GJBOW1 <gregg.bowen@gmail.com>
2023-08-19 15:41:23 +0200

    notes as of 19 August 2023

--- python_notes.py
+++ python_notes.py
@@ -1042,3 +1042,143 @@
 # my_shoe.cut_price_by(10)
 # print(my_shoe.price)
 
+# *Class vs Instance
+# We're now accustomed to designating instance attributes inside the constructor, the __init__() method. 
+# However, we can also declare and set attributes that don't belong to a single instance but to the 
+# class itself. Likewise, normally when we create a method on a class we pass in self to refer to the 
+# instance of the object.  These normal methods are referred to as instance methods.  But there are 
+# other types of methods we can implement on the class.  Methods that belong to the class and not 
+# the instance.
+
+# *Class Attributes
+# Class attributes are defined outside of any instance methods, and is shared among all instances 
+# of the class.  
+
+# class BankAccount:
+#     # Declaring a class attribute
+#     # Shared among all bank accounts
+#     bank_name = "First National Dojo"		
+
+#     def __init__(self, int_rate, balance):
+#         self.int_rate = int_rate
+#         self.balance = balance
+
+# Class attributes are more flexible because we can change them on an instance or the entire class.
+
+# Changing them on an instance:  
+# adriensAccount = BankAccount()
+# sadiesAccount = BankAccount()
+# adriensAccount.bank_name = "Dojo Credit Union"
+
+# print(adriensAccount.bank_name)
+
+# # output: Dojo Credit Union
+
+# print(sadiesAccount.bank_name)
+# # output: First National Dojo
+
+# Changing them on the entire class:
+
+# BankAccount.bank_name = "Bank of Dojo"
+
+# print(adriensAccount.bank_name)
+# # output: Bank of Dojo
+
+# print(sadiesAccount.bank_name)
+# # output: Bank of Dojo
+
+# *@classmethod
+# Class methods are defined with a decorator @classmethod. They belong to the class itself instead of the instance. 
+# Instead of implicitly passing self into the method, we pass cls. This is reference to the class.
+
+# This is how we write @classmethods:
+
+# class BankAccount:
+#     # class attributes
+#     bank_name = "First National Dojo"
+#     # new class attribute - a list of all the accounts!
+#     all_accounts = []
+
+#     def __init__(self, int_rate,balance):
+#         self.int_rate = int_rate
+#         self.balance = balance
+#         BankAccount.all_accounts.append(self)
+#     # class method to change the name of the bank
+#     @classmethod
+#     def change_bank_name(cls,name):
+#         cls.bank_name = name
+#     # class method to get balance of all accounts
+#     @classmethod
+#     def all_balances(cls):
+#         sum = 0
+#         # we use cls to refer to the class
+#         for account in cls.all_accounts:
+#             sum += account.balance
+#         return sum
+
+# It's important to know that class methods have no access to the instance attribute or any attribute that 
+# starts with self.
+
+# *@staticmethod
+# Static methods are functions defined within the class with a decorator @staticmethod.  They have no access on instance 
+# or class attributes, so if we need any existing values, they need to be passed in as arguments.
+
+# If we wanted our BankAccount class to have a utility function to add or subtract we could implement @staticmethod on 
+# the class.
+
+# class BankAccount:
+#     # ... __init__ goes here
+#     def with_draw(self,amount):
+#         # we can use the static method here to evaluate
+#         # if we can with draw the funds without going negative
+#         if BankAccount.can_withdraw(self.balance,amount):
+#             self.balance -= amount
+#         else:
+#             print("Insufficient Funds")
+#         return self
+#     # static methods have no access to any attribute
+#     # only to what is passed into it
+#     @staticmethod
+#     def can_withdraw(balance,amount):
+#     	if (balance - amount) < 0:
+#             return False
+#         else:
+#             return True
+
+# In Python, static methods offer us the opportunity to organize our code in a better way. We could do a simple 
+# check to see if the account can be withdrawn from, but what if we want to scale up our application with more 
+# logic around this idea? Well then we would have to update everywhere we are making that evaluation, but if we 
+# put it in a @staticmethod, then we can update all the checks from one place. And our code becomes a bit more D.R.Y.
+
+# *Association Between Classes:
+# Now that we have a User class and a BankAccount class, let's think about the relationship between them: a user 
+# has a bank account, or, in OOP terms, there is an association between these two classes. What this means is that, 
+# instead of keeping track of a balance directly in the User class, we'll encapsulate all the bank account information 
+# and associate a user with a specific instance of a Bank Account.
+
+# To keep things simple, let's start by assuming that each user has just one account that starts with a $0 balance 
+# and an interest rate of 2%. This means that the User class, instead of directly having a balance attribute, will 
+# now have an attribute of type BankAccount. To establish this relationship, we can update our User's __init__ method 
+# to something like this, removing the account_balance attribute and adding an account attribute:
+
+# class User:
+#     def __init__(self, name, email):
+#         self.name = name
+#         self.email = email
+#         self.account = BankAccount(int_rate=0.02, balance=0)	# added this line
+
+# Note: The BankAccount class should be in the same file as the User class, so the reference to it is recognized. Take 
+# a look into modularization if you feel the need to have the 2 classes in separate files.
+
+# We interact with this new attribute just as we do with previous attributes--the only difference is that we have 
+# personally defined the functionality of this class! We know the attributes and methods available to the account 
+# attribute by looking at our BankAccount class.
+
+# class User:
+#     def example_method(self):
+#     	# we can call the BankAccount instance's methods
+#         self.account.deposit(100)
+#     	# or access its attributes
+#     	print(self.account.balance
+
+
