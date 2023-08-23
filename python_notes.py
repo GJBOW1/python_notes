8fcbf09e102215c2509fd9fa1ae4355d97d5553e
GJBOW1 <gregg.bowen@gmail.com>
2023-08-23 18:23:11 +0200

    notes as of 23 August 2023

--- python_notes.py
+++ python_notes.py
@@ -1181,4 +1181,118 @@
 #     	# or access its attributes
 #     	print(self.account.balance
 
+# *Making Object Instances from Dictionary Data
+
+# Consider the following dictionary:
+# kevin = {"name": "Kevin Durant", "age":34, "position": "small forward", "team": "Brooklyn Nets"}
+
+# Let's say you were receiving data from an external source like a data base, and wanted to turn this dictionary data 
+# into a Player object so you could write some useful methods associated with players. You can imagine just from the 
+# way the dictionary is structured that you might write your class like this:
+
+# class Player:
+#     def __init__(self, name, age, position, team):
+#         self.name = name
+#         self.age = age
+#         self.position = position
+#         self.team = team
+
+# Okay, now that we have a class defined, let's take our dictionary info to make a Player instance.
+
+# kevin = {"name": "Kevin Durant", "age":34, "position": "small forward", "team": "Brooklyn Nets"}
+
+# # Pass in all the values from the dictionary by their keys
+# player_kevin = Player(kevin["name"], kevin["age"], kevin["position"], kevin["team"])
+# print(player_kevin.position) # prints small forward
+
+# Now you have a Player object, that you can write methods for! You can also make more player instances if you have 
+# more data that comes back in that format.
+
+# *Other Fun Facts About Dictionaries
+
+# *They're Fast
+# You are already familiar with Python dictionaries, but almost every language has a built-in key-value pair data 
+# structure. They are widely used, extremely powerful and considered fundamental for one primary reason:
+
+# They provide constant-time lookup, insertion and deletion on average
+# Many extremely common problems are best solved with the key-value pair data structure (commonly called a hash 
+# table in computer science.) Some examples: Dealing with uniqueness of data, frequency of values, and finding 
+# duplicates in data sets.
+
+# *Also Called a Hash Table
+# Here are some of the names you will hear them called.
+
+# Dictionaries	            Python
+# Objects (misleadingly!)	    JavaScript, JSON
+# Maps, Hash Maps	            Java, C++
+# Hash Table	                C#
+# Associative Array	        PHP
+
+# We often use Object Oriented Programming for web development because we want to work more efficiently and write less code. In order to achieve better code, we use the 4 pillars of OOP.
+
+# *Benefits of using OOP:
+# Implements D.R.Y. (Don't Repeat Yourself) code
+# Makes our application scalable.
+# Makes our code reusable.
+# Makes our applications easily maintainable.
+
+# *1. Encapsulation
+# Encapsulation is the idea that we can group code together into objects; hence Object Oriented Programming. We use 
+# classes or "coding blue prints" to define what our objects are and how they behave. We encapsulate attributes and 
+# methods in our class.
+
+# Example:
+# class CoffeeM:
+#     def __init__(self,name):
+#         self.name = name
+#         self.water_temp = 200
+#     def brew_now(self,beans):
+#         print(f"Using {beans}!")
+#         print("Brew now brown cow!")
+#     def clean(self):
+#         print("Cleaning!")
+
+# *2. Inheritance
+# Inheritance is the idea that we pass along attributes and methods from one class into a "sub-class" or child class, 
+# and not have to re-write the code to make it work. Child classes can be more specific versions of their Parent class. 
+# Using the key word "super" will call methods
+
+# Example:
+# class CappuccinoM( CoffeeM ):
+#     def __init__(self,name):
+#         super().__init__(name)
+#         self.milk = "whole"
+#     def make_cappuccino(self,beans):
+#         super().brew_now(beans)
+#         print("Frothy!!!")
+
+# *3. Polymorphism
+# Polymorphism means "many forms", and the idea in OOP is that a Child class can have a different version of a method 
+# than the Parent class. In this example the child class of CappuccinoM has a clean method, and so does CoffeeM. 
+# Depending on the class, the clean method will do different things.
+
+# Example:
+# class CappuccinoM( CoffeeM ):
+#     def __init__(self,name):
+#         super().__init__(name)
+#         self.milk = "whole"
+#     def make_cappuccino(self,beans):
+#         super().brew_now(beans)
+#         print("Frothy!!!")
+#     def clean(self):
+#         print("Cleaning the froth!")
+
+# *4. Abstraction
+# Abstraction is an extension of Encapsulation, and we can hide attributes or methods that a Barista doesn't need to 
+# know about, like a CoffeeM. That way the Barista can make a cup of coffee in a simpler manner.
+
+# Example: 
+# class Barista:
+#     def __init__(self,name):
+#         self.name = name
+#         self.cafe = CoffeeM("Cafe")
+#     def make_coffee(self, beans):
+#         self.cafe.brew_now(beans)
+
+
 
